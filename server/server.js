15babe1fa6a01d94761cc8e9e7bb8e4f2d43de88
Oscar Silva <oscarsilva2356@gmail.com>
2023-06-30 17:51:30 -0700

    Fixed the env issue

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+app.get("/", (req, res) => {
+  console.log("get all restaurants");
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
