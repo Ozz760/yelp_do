d716213284e8600c35c3ecd66dda6ff3dae8c144
Oscar Silva <oscarsilva2356@gmail.com>
2023-07-03 19:40:57 -0700

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Router>
+        <Route exact path="/"></Route>
+      </Router>
     </div>
   );
 };
