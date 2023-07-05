86ad3311f508e29cd4dd1b687868e192df7ce7db
Oscar Silva <oscarsilva2356@gmail.com>
2023-07-05 16:52:50 -0700

    changed to top bar

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -2,17 +2,25 @@ import React from "react";
 
 const AddRestaurant = () => {
   return (
-    <div className=" mb-4">
+    <div className="mb-4">
       <form action="">
-        <div className="columns-3 gap-8 text-center">
+        <div className="columns-4 gap-8 text-center mx-4">
           <div className="">
-            <input type="text" className="" placeholder="Name" />
+            <input
+              type="text"
+              className="rounded border-2 h-10 border-gray-300 w-full pl-2"
+              placeholder="Name"
+            />
           </div>
           <div className="">
-            <input type="text" className="" placeholder="Location" />
+            <input
+              type="text"
+              className="rounded border-2 h-10 border-gray-300 w-full pl-2"
+              placeholder="Location"
+            />
           </div>
-          <div className="col">
-            <select className="" placeholder="Price Range">
+          <div className="">
+            <select className="rounded border-2 border-gray-300 w-full h-10">
               <option disabled>Price Range</option>
               <option value="1">$</option>
               <option value="2">$$</option>
@@ -20,6 +28,11 @@ const AddRestaurant = () => {
               <option value="4">$$$$</option>
               <option value="5">$$$$$</option>
             </select>
+          </div>
+          <div>
+            <button className=" rounded border bg-blue-600 text-white w-full h-10">
+              Add
+            </button>
           </div>
         </div>
       </form>
