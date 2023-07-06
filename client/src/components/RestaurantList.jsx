e266db7e68c7c9f2ae895bf4296edd7dc30d8bed
Oscar Silva <oscarsilva2356@gmail.com>
2023-07-05 23:00:21 -0700

    added list

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const RestaurantList = () => {
-  return <div>RestaurantList</div>;
+  return (
+    <div className="mt-10">
+      <table className="table-auto">
+        <thread>
+          <tr>
+            <th className="">Restaurant</th>
+            <th className="">Location</th>
+            <th className="">Price Range</th>
+            <th className="">Ratings</th>
+            <th className="">Edit</th>
+            <th className="">Delete</th>
+          </tr>
+        </thread>
+        <tbody>
+          <tr>
+            <td className="">McDonalds</td>
+            <td className="">San Francisco</td>
+            <td className="">$$</td>
+            <td className="">Update</td>
+            <td className="">Delete</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default RestaurantList;
