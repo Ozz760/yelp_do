70231173493c8c68b22edc435a60f11bd1423f6f
Oscar Silva <oscarsilva2356@gmail.com>
2023-07-16 16:21:02 -0700

    added updated page

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,34 +1,79 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import { useNavigate } from "react-router-dom";
 
 const UpdateRestaurant = (props) => {
   const { id } = useParams();
+  const { restaurants } = useContext(RestaurantsContext);
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
   const [priceRange, setPriceRange] = useState("Price Range");
+
+  // Destructure the navigate function from the react-router-dom.
+  let navigate = useNavigate();
+
+  // Function to handle the update button.
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await RestaurantFinder.get(`/${id}`);
+        setName(response.data.data.restaurant.name);
+        setLocation(response.data.data.restaurant.location);
+        setPriceRange(response.data.data.restaurant.price_range);
+      };
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  // Function to handle the update button.
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const updatedRestaurant = await RestaurantFinder.put(`/${id}`, {
+        name: name,
+        location: location,
+        price_range: priceRange,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <form action="">
         <div className="px-12 m-10">
-          <label>Name</label>
+          <label htmlFor="name">Name</label>
           <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             type="text"
             className="rounded border-2 pl-2 border-gray-300 w-full h-10 focus:border-black focus:outline-none"
           />
         </div>
 
         <div className="px-12 m-10">
-          <label>Location</label>
+          <label htmlFor="location">Location</label>
           <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
             type="text"
             className="rounded border-2 pl-2 border-gray-300 w-full h-10 focus:border-black focus:outline-none"
           />
         </div>
 
         <div className="px-12 m-10">
-          <label>Price Range</label>
-          <select className="rounded border-2 border-gray-300 w-full h-10 focus:border-black focus:outline-none">
+          <label htmlFor="princeRange">Price Range</label>
+          <select
+            value={priceRange}
+            onClick={(e) => setPriceRange(e.target.value)}
+            className="rounded border-2 border-gray-300 w-full h-10 focus:border-black focus:outline-none"
+          >
             <option disabled>Price Range</option>
             <option value="1">$</option>
             <option value="2">$$</option>
@@ -38,7 +83,11 @@ const UpdateRestaurant = (props) => {
           </select>
         </div>
         <div className="px-12 m-10">
-          <button className="rounded border bg-blue-600 text-white hover:bg-blue-700 p-3 w-20">
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="rounded border bg-blue-600 text-white hover:bg-blue-700 p-3 w-20"
+          >
             Submit
           </button>
         </div>
