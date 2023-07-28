8cc439c6bed3f2d3390564db66caae4a597d8975
Oscar Silva <oscarsilva2356@gmail.com>
2023-07-28 14:11:48 -0700

    added state to reviwes

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddReview = () => {
+  const [name, setName] = useState("");
+  const [reviewText, setReviewText] = useState("");
+  const [rating, setRating] = useState("Rating");
+
   return (
     <div>
       <form action="">
         <div className="px-4 m-10">
           <label htmlFor="name">Name</label>
           <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             id="name"
             type="text"
             className="rounded border-2 pl-2 border-gray-300 w-full h-10 focus:border-black focus:outline-none"
           />
         </div>
-
         <div className="px-4 m-10">
           <label htmlFor="rating">Rating</label>
           <select
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
             id="rating"
             className="rounded border-2 pl-2 border-gray-300 w-full h-10 focus:border-black focus:outline-none"
           >
@@ -27,7 +34,15 @@ const AddReview = () => {
             <option value="5">5</option>
           </select>
         </div>
-
+        <div className="px-14">
+          <label htmlFor="review">Review</label>
+          <textarea
+            value={reviewText}
+            onChange={(e) => setReviewText(e.target.value)}
+            id="Reviews"
+            className="rounded border-2 border-gray-300 w-full focus:border-black focus:outline-none pl-2 pt-1"
+          ></textarea>
+        </div>
         <div className="px-4 m-10">
           <button
             type="submit"
